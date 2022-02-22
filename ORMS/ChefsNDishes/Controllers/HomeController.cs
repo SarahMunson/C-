2cd1423ec115aeb8ee2d341b3b5dbbcc466fcc6f
John Doe <s.maeflower@gmail.com>
2022-02-22 13:45:57 -0600

    chefs less validations

---
+++ ORMS/ChefsNDishes/Controllers/HomeController.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ChefsNDishes.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ChefsNDishes.Controllers
+{
+    public class HomeController : Controller
+    {
+        private MyContext _context;
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger, MyContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            ViewBag.AllChefs = _context.Chefs.Include(d => d.Dish).ToList();
+            return View();
+        }
+
+        [HttpGet("addChef")]
+        public IActionResult addChef()
+        {
+            return View();
+        }
+
+        [HttpPost("addChef")]
+        public IActionResult addNewChef(Chef newChef)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Chefs.Add(newChef);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            } else {
+                return View("addChef");
+            }
+        }
+
+        [HttpGet("dishes")]
+        public IActionResult Dishes()
+        {
+            ViewBag.allDishes = _context.Dishes.ToList();
+            ViewBag.AllChefs = _context.Chefs.Include(d => d.Dish).ToList();
+            return View();
+        }
+
+        [HttpGet("newDish")]
+        public IActionResult newDish()
+        {
+            ViewBag.AllChefs = _context.Chefs.ToList();
+            return View();
+        }
+
+        [HttpPost("newDish")]
+        public IActionResult addNewDish(Dish newDish)
+        {
+            if(ModelState.IsValid)
+            {
+                _context.Dishes.Add(newDish);
+                _context.SaveChanges();
+                return RedirectToAction("Dishes");
+            } else {
+                ViewBag.AllChefs = _context.Chefs.Include(d => d.Dish).ToList();
+                return View("newDish");
+            }
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ORMS/ChefsNDishes/Models/Chef.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+namespace ChefsNDishes.Models
+{
+    public class Chef
+    {
+        [Key]
+        public int ChefId { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        public DateTime DOB { get; set; }
+        public List<Dish> Dish { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public DateTime UpdatedAt { get; set; } = DateTime.Now;
+    }
+}
